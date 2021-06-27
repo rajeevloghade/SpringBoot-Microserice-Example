aad3571ad4b4f6dba4ddf9155a46b453f214aa4a
Rajeev Loghade <rajeevloghade010@gmail.com>
2021-06-27 17:10:27 +0530

    load balancing with naming server

--- feign-client-service/src/main/java/io/javabrains/model/ISimpleClient.java
+++ feign-client-service/src/main/java/io/javabrains/model/ISimpleClient.java
@@ -1,12 +1,14 @@
 package io.javabrains.model;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "simple-client", url = "http://localhost:8085")
+@FeignClient(name = "feign-client-data"/* , url = "http://localhost:9001" */ )
+@RibbonClient(name = "feign-client-data")
 public interface ISimpleClient {
 
 	@GetMapping(path = "/data/{dataId}", produces = "application/json")
