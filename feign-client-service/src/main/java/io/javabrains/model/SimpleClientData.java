9b0d796eb263b9a565c2195d4e2d8aaa5340c1a2
rajeevloghade <rajeevloghade010@gmail.com>
2021-06-28 11:00:25 +0530

    added member added

--- feign-client-service/src/main/java/io/javabrains/model/SimpleClientData.java
+++ feign-client-service/src/main/java/io/javabrains/model/SimpleClientData.java
@@ -1,20 +1,21 @@
 package io.javabrains.model;
 
 public class SimpleClientData {
-
 	private int id;
 	private String name;
 	private int amount;
+	private String port;
 
 	public SimpleClientData() {
 		super();
 	}
 
-	public SimpleClientData(int id, String name, int amount) {
+	public SimpleClientData(int id, String name, int amount, String port) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.amount = amount;
+		this.port = port;
 	}
 
 	public int getId() {
@@ -29,9 +30,13 @@ public class SimpleClientData {
 		return amount;
 	}
 
+	public String getPort() {
+		return port;
+	}
+
 	@Override
 	public String toString() {
-		return "SimpleClientData [id=" + id + ", name=" + name + ", amount=" + amount + "]";
+		return "SimpleClientData [id=" + id + ", name=" + name + ", amount=" + amount + ", port=" + port + "]";
 	}
 
 }
